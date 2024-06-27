d87f5359bf6ebb1fde969d1987786264033cfa65
someone5678 <someone5678@users.noreply.github.com>
2024-06-27 20:20:02 +0900

    certification: Add support for private Keybox.java
    Change-Id: I71c37bd1d4747d28b546e98043eb765cbcdaa83f

--- setup-buildfiles.sh
+++ setup-buildfiles.sh
@@ -7,11 +7,18 @@ certif_root=$(pwd)
 fi
 configs_dir="$certif_root/configs"
 service_file="$certif_root/system.prop"
+service_java_file="$certif_root/libs/src/com/android/internal/util/custom/certification/Keybox.java"
 fields_file="$configs_dir/pif.json"
 fields_file_public="$configs_dir/pif_public.json"
+fields_java_file="$configs_dir/Keybox.java"
+fields_java_file_public="$configs_dir/Keybox_public.java"
 
 if [ ! -f $fields_file ]; then
   fields_file=$fields_file_public
+fi
+
+if [ ! -f $fields_java_file ]; then
+  fields_java_file=$fields_java_file_public
 fi
 
 get_field() {
@@ -27,6 +34,8 @@ persist.sys.pihooks.security_patch=$(get_field SECURITY_PATCH)
 persist.sys.pihooks.manufacturer=$(get_field MANUFACTURER)
 persist.sys.pihooks.api_level=$(get_field DEVICE_INITIAL_SDK_INT)
 EOF
+
+cp -rf $fields_java_file $service_java_file
 }
 
 generate_file
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@
 if [ -z $VENDOR_CERTIFICATION_SETUP_DONE ]; then
 
 export run_from_vendorsetup=true
-bash vendor/certification/setup-buildprops.sh
+bash vendor/certification/setup-buildfiles.sh
 unset run_from_vendorsetup
 
 export VENDOR_CERTIFICATION_SETUP_DONE=true
