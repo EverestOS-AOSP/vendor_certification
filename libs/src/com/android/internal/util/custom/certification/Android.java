05ffcc6bb11833659475acf7bfc9b51fb30560b4
someone5678 <someone5678@users.noreply.github.com>
2024-08-25 22:22:41 +0900

    certification: libs: Clean-up before changes
    Change-Id: I7d50991126dc70966c4edfdecc6096ba3fa7bb5a

--- libs/src/com/android/internal/util/custom/certification/Android.java
+++ libs/src/com/android/internal/util/custom/certification/Android.java
@@ -9,28 +9,6 @@ import android.security.keystore.KeyProperties;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.android.internal.org.bouncycastle.asn1.ASN1Boolean;
-import com.android.internal.org.bouncycastle.asn1.ASN1Encodable;
-import com.android.internal.org.bouncycastle.asn1.ASN1EncodableVector;
-import com.android.internal.org.bouncycastle.asn1.ASN1Enumerated;
-import com.android.internal.org.bouncycastle.asn1.ASN1ObjectIdentifier;
-import com.android.internal.org.bouncycastle.asn1.ASN1OctetString;
-import com.android.internal.org.bouncycastle.asn1.ASN1Sequence;
-import com.android.internal.org.bouncycastle.asn1.ASN1TaggedObject;
-import com.android.internal.org.bouncycastle.asn1.DEROctetString;
-import com.android.internal.org.bouncycastle.asn1.DERSequence;
-import com.android.internal.org.bouncycastle.asn1.DERTaggedObject;
-import com.android.internal.org.bouncycastle.asn1.x509.Extension;
-import com.android.internal.org.bouncycastle.cert.X509CertificateHolder;
-import com.android.internal.org.bouncycastle.cert.X509v3CertificateBuilder;
-import com.android.internal.org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import com.android.internal.org.bouncycastle.openssl.PEMKeyPair;
-import com.android.internal.org.bouncycastle.openssl.PEMParser;
-import com.android.internal.org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import com.android.internal.org.bouncycastle.operator.ContentSigner;
-import com.android.internal.org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
-import com.android.internal.org.bouncycastle.util.io.pem.PemReader;
-
 import java.io.ByteArrayInputStream;
 import java.io.StringReader;
 import java.lang.reflect.Field;
@@ -56,12 +34,7 @@ public final class Android {
     private static Boolean sEnableCertHook =
             SystemProperties.getBoolean("persist.sys.certhook.enable", true);
 
-    private static final PEMKeyPair EC, RSA;
-    private static final ASN1ObjectIdentifier OID = new ASN1ObjectIdentifier("1.3.6.1.4.1.11129.2.1.17");
-    private static final List<Certificate> EC_CERTS = new ArrayList<>();
-    private static final List<Certificate> RSA_CERTS = new ArrayList<>();
     private static final HashMap<String, Object> map;
-    private static final CertificateFactory certificateFactory;
 
     private static final String cert_device = SystemProperties.get("persist.sys.pihooks.device", "");
     private static final String cert_fp = SystemProperties.get("persist.sys.pihooks.fingerprint", "");
@@ -93,20 +66,6 @@ public final class Android {
         if (!cert_hardware.isEmpty()) tMap.put("HARDWARE", cert_hardware);
         if (cert_sdk != 0) tMap.put("DEVICE_INITIAL_SDK_INT", cert_sdk);
         map = new HashMap<>(tMap);
-        try {
-            certificateFactory = CertificateFactory.getInstance("X.509");
-
-            EC = parseKeyPair(Keybox.EC.PRIVATE_KEY);
-            EC_CERTS.add(parseCert(Keybox.EC.CERTIFICATE_1));
-            EC_CERTS.add(parseCert(Keybox.EC.CERTIFICATE_2));
-
-            RSA = parseKeyPair(Keybox.RSA.PRIVATE_KEY);
-            RSA_CERTS.add(parseCert(Keybox.RSA.CERTIFICATE_1));
-            RSA_CERTS.add(parseCert(Keybox.RSA.CERTIFICATE_2));
-        } catch (Throwable t) {
-            Log.e(TAG, t.toString());
-            throw new RuntimeException(t);
-        }
     }
 
     public static boolean isCertHookEnabled() {
@@ -115,18 +74,6 @@ public final class Android {
 
     public static boolean isCertifiedPropsEmpty() {
         return map.isEmpty();
-    }
-
-    private static PEMKeyPair parseKeyPair(String key) throws Throwable {
-        try (PEMParser parser = new PEMParser(new StringReader(key))) {
-            return (PEMKeyPair) parser.readObject();
-        }
-    }
-
-    private static Certificate parseCert(String cert) throws Throwable {
-        try (PemReader reader = new PemReader(new StringReader(cert))) {
-            return certificateFactory.generateCertificate(new ByteArrayInputStream(reader.readPemObject().getContent()));
-        }
     }
 
     private static Field getField(String fieldName) {
@@ -212,86 +159,8 @@ public final class Android {
             if (isCallerSafetyNet()) {
                 throw new UnsupportedOperationException();
             }
-            return caList;
         }
-        try {
-            X509Certificate leaf = (X509Certificate) certificateFactory.generateCertificate(new ByteArrayInputStream(caList[0].getEncoded()));
 
-            byte[] bytes = leaf.getExtensionValue(OID.getId());
-
-            if (bytes == null) return caList;
-
-            X509CertificateHolder holder = new X509CertificateHolder(leaf.getEncoded());
-
-            Extension ext = holder.getExtension(OID);
-
-            ASN1Sequence sequence = ASN1Sequence.getInstance(ext.getExtnValue().getOctets());
-
-            ASN1Encodable[] encodables = sequence.toArray();
-
-            ASN1Sequence teeEnforced = (ASN1Sequence) encodables[7];
-
-            ASN1EncodableVector vector = new ASN1EncodableVector();
-
-            for (ASN1Encodable asn1Encodable : teeEnforced) {
-                ASN1TaggedObject taggedObject = (ASN1TaggedObject) asn1Encodable;
-                if (taggedObject.getTagNo() == 704) continue;
-                vector.add(taggedObject);
-            }
-
-            LinkedList<Certificate> certificates;
-
-            X509v3CertificateBuilder builder;
-            ContentSigner signer;
-
-            if (KeyProperties.KEY_ALGORITHM_EC.equals(leaf.getPublicKey().getAlgorithm())) {
-                certificates = new LinkedList<>(EC_CERTS);
-                builder = new X509v3CertificateBuilder(new X509CertificateHolder(EC_CERTS.get(0).getEncoded()).getSubject(), holder.getSerialNumber(), holder.getNotBefore(), holder.getNotAfter(), holder.getSubject(), EC.getPublicKeyInfo());
-                signer = new JcaContentSignerBuilder(leaf.getSigAlgName()).build(new JcaPEMKeyConverter().getPrivateKey(EC.getPrivateKeyInfo()));
-            } else {
-                certificates = new LinkedList<>(RSA_CERTS);
-                builder = new X509v3CertificateBuilder(new X509CertificateHolder(RSA_CERTS.get(0).getEncoded()).getSubject(), holder.getSerialNumber(), holder.getNotBefore(), holder.getNotAfter(), holder.getSubject(), RSA.getPublicKeyInfo());
-                signer = new JcaContentSignerBuilder(leaf.getSigAlgName()).build(new JcaPEMKeyConverter().getPrivateKey(RSA.getPrivateKeyInfo()));
-            }
-
-            byte[] verifiedBootKey = new byte[32];
-            byte[] verifiedBootHash = new byte[32];
-
-            ThreadLocalRandom.current().nextBytes(verifiedBootKey);
-            ThreadLocalRandom.current().nextBytes(verifiedBootHash);
-
-            ASN1Encodable[] rootOfTrustEnc = {new DEROctetString(verifiedBootKey), ASN1Boolean.TRUE, new ASN1Enumerated(0), new DEROctetString(verifiedBootHash)};
-
-            ASN1Sequence rootOfTrustSeq = new DERSequence(rootOfTrustEnc);
-
-            ASN1TaggedObject rootOfTrustTagObj = new DERTaggedObject(704, rootOfTrustSeq);
-
-            vector.add(rootOfTrustTagObj);
-
-            ASN1Sequence hackEnforced = new DERSequence(vector);
-
-            encodables[7] = hackEnforced;
-
-            ASN1Sequence hackedSeq = new DERSequence(encodables);
-
-            ASN1OctetString hackedSeqOctets = new DEROctetString(hackedSeq);
-
-            Extension hackedExt = new Extension(OID, false, hackedSeqOctets);
-
-            builder.addExtension(hackedExt);
-
-            for (ASN1ObjectIdentifier extensionOID : holder.getExtensions().getExtensionOIDs()) {
-                if (OID.getId().equals(extensionOID.getId())) continue;
-                builder.addExtension(holder.getExtension(extensionOID));
-            }
-
-            certificates.addFirst(new JcaX509CertificateConverter().getCertificate(builder.build(signer)));
-
-            return certificates.toArray(new Certificate[0]);
-
-        } catch (Throwable t) {
-            Log.e(TAG, t.toString());
-        }
         return caList;
     }
 
