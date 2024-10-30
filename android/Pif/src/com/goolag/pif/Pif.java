8e6e3af3f9895194155e8415ef386f68158465ed
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-10-30 18:19:23 +0900

    Pif: Adapt to this project
    * Adapt to our implementation
    * Add category for "*_for_attestation" props
    * Simplify code
    
    Change-Id: Idc44a777fbcc3a589062197b957df6e10825d572

--- android/Pif/src/com/goolag/pif/Pif.java
+++ android/Pif/src/com/goolag/pif/Pif.java
@@ -11,81 +11,67 @@ import android.os.Bundle;
 import android.provider.Settings;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
-import com.android.internal.util.evolution.PixelPropsUtils;
 import com.android.settingslib.widget.TopIntroPreference;
 
 import com.goolag.pif.R;
+
+import com.android.internal.util.custom.certification.Android;
+import com.android.internal.util.custom.certification.Android.PiHookProperties;
 
 public class Pif extends PreferenceFragmentCompat
         implements Preference.OnPreferenceChangeListener {
 
     private TopIntroPreference mIntroPreference;
 
-    private Preference mManufacturerPreference;
-    private Preference mModelPreference;
-    private Preference mFingerprintPreference;
-    private Preference mBrandPreference;
-    private Preference mProductPreference;
-    private Preference mDevicePreference;
-    private Preference mReleasePreference;
-    private Preference mIDPreference;
-    private Preference mIncrementalPreference;
-    private Preference mTypePreference;
-    private Preference mTAGSPreference;
-    private Preference mSecurityPatchPreference;
-    private Preference mDeviceInitialSdkIntPreference;
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.main, rootKey);
 
-        String selectedArrayName = Settings.System.getString(
-                getContext().getContentResolver(), Settings.System.PPU_SPOOF_BUILD_GMS_ARRAY);
-
-        int selectedArrayResId = getResources().getIdentifier(selectedArrayName, "array", getContext().getPackageName());
-
-        String[] selectedDeviceProps = getResources().getStringArray(selectedArrayResId);
-
         mIntroPreference = findPreference("device_intro");
-
-        mManufacturerPreference = findPreference("manufacturer");
-        mModelPreference = findPreference("model");
-        mFingerprintPreference = findPreference("fingerprint");
-        mBrandPreference = findPreference("brand");
-        mProductPreference = findPreference("product");
-        mDevicePreference = findPreference("device");
-        mReleasePreference = findPreference("release");
-        mIDPreference = findPreference("id");
-        mIncrementalPreference = findPreference("incremental");
-        mTypePreference = findPreference("type");
-        mTAGSPreference = findPreference("tags");
-        mSecurityPatchPreference = findPreference("security_patch");
-        mDeviceInitialSdkIntPreference = findPreference("device_initial_sdk_int");
-
         mIntroPreference.setTitle(Build.MANUFACTURER + " " + Build.MODEL);
 
-        mManufacturerPreference.setSummary(selectedDeviceProps[0]);
-        mModelPreference.setSummary(selectedDeviceProps[1]);
-        mFingerprintPreference.setSummary(selectedDeviceProps[2]);
-        mBrandPreference.setSummary(selectedDeviceProps[3]);
-        mProductPreference.setSummary(selectedDeviceProps[4]);
-        mDevicePreference.setSummary(selectedDeviceProps[5].isEmpty() ?
-                PixelPropsUtils.getDeviceName(selectedDeviceProps[2]) : selectedDeviceProps[5]);
-        mReleasePreference.setSummary(selectedDeviceProps[6]);
-        mIDPreference.setSummary(selectedDeviceProps[7].isEmpty() ?
-                PixelPropsUtils.getBuildID(selectedDeviceProps[5]) : selectedDeviceProps[7]);
-        mIncrementalPreference.setSummary(selectedDeviceProps[8]);
-        mTypePreference.setSummary(selectedDeviceProps[9].isEmpty() ? "user" : selectedDeviceProps[9]);
-        mTAGSPreference.setSummary(selectedDeviceProps[10].isEmpty() ? "release-keys" : selectedDeviceProps[10]);
-        mSecurityPatchPreference.setSummary(selectedDeviceProps[11]);
-        mDeviceInitialSdkIntPreference.setSummary(selectedDeviceProps[12]);
+        setSummaryIfNotEmpty("device", false);
+        setSummaryIfNotEmpty("fingerprint", false);
+        setSummaryIfNotEmpty("product", false);
+        setSummaryIfNotEmpty("model", false);
+        setSummaryIfNotEmpty("brand", false);
+        setSummaryIfNotEmpty("security_patch", false);
+        setSummaryIfNotEmpty("manufacturer", false);
+        setSummaryIfNotEmpty("board", false);
+        setSummaryIfNotEmpty("hardware", false);
+        setSummaryIfNotEmpty("device_initial_sdk_int", false);
+        setSummaryIfNotEmpty("release", false);
+        setSummaryIfNotEmpty("id", false);
+        setSummaryIfNotEmpty("incremental", false);
+        setSummaryIfNotEmpty("type", false);
+        setSummaryIfNotEmpty("tags", false);
+        // *_for_attestation
+        if (!setSummaryIfNotEmpty("device", true)
+            && !setSummaryIfNotEmpty("product", true)
+            && !setSummaryIfNotEmpty("model", true)
+            && !setSummaryIfNotEmpty("brand", true)
+            && !setSummaryIfNotEmpty("manufacturer", true)) {
+            getPreferenceScreen().removePreference(findPreference("attestation"));
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return true;
+    }
+
+    private boolean setSummaryIfNotEmpty(String prop, boolean attest) {
+        final Preference pref = findPreference(attest ? prop + "_for_attestation" : prop);
+        String ret = PiHookProperties.get(prop, "", attest);
+        if (ret.isEmpty()) {
+            getPreferenceScreen().removePreference(pref);
+            return false;
+        }
+        pref.setSummary(ret);
         return true;
     }
 }
